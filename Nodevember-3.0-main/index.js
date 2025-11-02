36a52c700d863e78f48c637ddeb9fd9052cc227f
Delta-Student27 <tanushripatil26@gmail.com>
2025-11-02 19:14:54 +0530

    Configured for Vercel deployment

--- Nodevember-3.0-main/index.js
+++ Nodevember-3.0-main/index.js
@@ -1,29 +1,26 @@
 import express from "express";
-import connectDB from "./config/db.js"
+import serverless from "serverless-http";
+import connectDB from "./config/db.js";
 
-import authRoutes from './routes/user.route.js'
-import bookRoutes from "./routes/book.route.js"
-import orderRoutes from "./routes/order.route.js"
-import aiRoutes from "./routes/ai.route.js"
+import authRoutes from "./routes/user.route.js";
+import bookRoutes from "./routes/book.route.js";
+import orderRoutes from "./routes/order.route.js";
+import aiRoutes from "./routes/ai.route.js";
 
 const app = express();
-
 connectDB();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Bookstore api is running!");
+  res.send("Bookstore API is running successfully on Vercel!");
 });
 
-app.use('/api/auth', authRoutes)
-app.use('/api/book', bookRoutes)
-app.use('/api/order', orderRoutes)
-app.use('/api/ai', aiRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/book", bookRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/ai", aiRoutes);
 
-
-app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:3000`);
-});
+// 👇 This replaces app.listen()
+export const handler = serverless(app);
